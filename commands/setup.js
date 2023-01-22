b7e1cd66d5ed484ff916de92ba8fbb1e3e08a09d
nevindra <nevindraibnazhifi@gmail.com>
2023-01-22 17:26:45 +0700

    finish setup commands to initial setup server

--- commands/setup.js
+++ commands/setup.js
@@ -4,11 +4,25 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName('setup')
         .setDescription('Initial setup for the server')
-        .addUserOption(option => option
-            .setName('channels')
-            .setDescription('Set the channel to use')
-            .setRequired(true)),
+        .addChannelOption(opt =>
+            opt.setName('channels')
+                .setDescription('Set the channel to use')
+                .setRequired(true)
+                .addChannelTypes(0) // 0 = Text channel
+        ),
     async execute(interaction) {
-        await interaction.reply('Asik!');
+        // Get channel ID from the user
+        let channel = interaction.options.getChannel('channels');
+        await interaction.reply('Registering your channel...');
+        // Send a message to channel based on the ID given before
+        try {
+            // send a message to the given channel ID and then delete the message.
+            let msg = await channel.send('This channel will be used to inform you about the new updates.')
+            setInterval(async () => {
+                await msg.delete()
+            }, 500)
+        } catch (e) {
+            throw e
+        }
     },
 };
